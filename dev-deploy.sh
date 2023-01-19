bdfb3d9fe0a848fad3cefbac922de114e965e77c
tonmoy <tonmoy@sheba.xyz>
2023-01-19 15:25:28 +0600

    DASHBOARD DESIGN

---
+++ dev-deploy.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env sh
+git pull
+docker rm -f drugn
+docker build --no-cache --label drugn -t drugn:latest .
+docker run -p 80:80 -d --name drugn drugn
+docker rmi $(docker images -f dangling=true -q)
--- src/components/home/dashboard/dashboard-nft.tsx
+++ src/components/home/dashboard/dashboard-nft.tsx
@@ -5,17 +5,21 @@ import {CustomIcon} from "../../../utils/custom-icon";
 import {Dimensions, StyleSheet, TouchableOpacity} from "react-native";
 import {useState} from "react";
 import {NftList} from "./nft-list";
+import {
+    PanGestureHandler,
+} from "react-native-gesture-handler";
+
 const {height: windowHeight} = Dimensions.get('window');
 
 const smallScreen = windowHeight < 650
-const styles=StyleSheet.create({
-    container:{
+const styles = StyleSheet.create({
+    container: {
         borderTopRightRadius: 20,
         borderTopLeftRadius: 20,
         zIndex: 10,
         width: "100%",
     },
-    handle_container:{
+    handle_container: {
         position: 'absolute',
         top: 10,
         display: 'flex',
@@ -24,27 +28,27 @@ const styles=StyleSheet.create({
         width: '100%',
         left: 0
     },
-    handle:{
+    handle: {
         backgroundColor: colors.onBackgroundSpace,
         width: 40,
         height: 5,
         borderRadius: 20,
         zIndex: 10
     },
-    card:{
+    card: {
         backgroundColor: colors.navBackground,
         borderRadius: 20,
         marginBottom: 0
     },
-    cardWrapper:{
+    cardWrapper: {
         flexDirection: "row",
         alignItems: "center",
         justifyContent: "center",
         position: 'relative'
     },
-    cardIcon:{flex: 1, alignItems: "center", flexDirection: "column"},
-    cardIconDivider:{width: 1, backgroundColor: colors.textDark, height: smallScreen?50:100},
-    cardIconText:{
+    cardIcon: {flex: 1, alignItems: "center", flexDirection: "column"},
+    cardIconDivider: {width: 1, backgroundColor: colors.textDark, height: smallScreen ? 50 : 100},
+    cardIconText: {
         fontFamily: "Montserrat_700Bold",
         marginTop: 10,
         fontSize: smallScreen ? 10 : 14
@@ -54,16 +58,28 @@ export const DashboardNft = () => {
     const theme = useTheme()
 
     const [open, setOpen] = useState(false)
+    const onSwipe = (event) => {
+        console.log(event.nativeEvent)
+        if (event.nativeEvent.velocityY > 10) {
+            setOpen(false)
+        }
+        if (event.nativeEvent.velocityY < -10) {
+            setOpen(true)
+        }
+    }
     return (
-        <FBox style={{...styles.container,
+        <FBox style={{
+            ...styles.container,
             backgroundColor: theme.colors.background,
             position: open ? "absolute" : "relative",
             height: open ? "100%" : "auto",
             top: open ? 80 : 0,
         }}>
-            <FBox style={styles.handle_container}>
-                <TouchableOpacity style={styles.handle} onPress={() => setOpen(!open)}/>
-            </FBox>
+            <TouchableOpacity style={styles.handle_container}>
+                <PanGestureHandler onEnded={onSwipe}>
+                    <TouchableOpacity style={styles.handle} onPress={() => setOpen(!open)}/>
+                </PanGestureHandler>
+            </TouchableOpacity>
             <FBox style={{margin: 18}}>
                 <Text style={{fontFamily: "Montserrat_700Bold", paddingBottom: 20}}>NFT</Text>
                 <Card theme={{elevation: 1}} style={styles.card}>
