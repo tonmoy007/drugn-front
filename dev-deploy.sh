55fc45f80713c73cb02078843ee195de3ee7e69a
Saddam Hossain <tonmoy007@users.noreply.github.com>
2023-03-08 06:02:49 +0600

    NEW CAMERA

--- dev-deploy.sh
+++ dev-deploy.sh
@@ -3,7 +3,7 @@ git pull
 set -a # automatically export all variables
 source "$(pwd)/.env"
 set +a
-docker build --no-cache --label drugn -t drugn:latest .
+docker build --no-cache --label drugn -t drugn:latest -f pro.Dockerfile .
 docker rm -f drugn
-docker run -p ${PORT}:19006 -d -v /etc/sysctl.conf:/etc/sysctl.conf -v "$(pwd)/external/expo-camera/build:/app/node_modules/expo-camera/build" --name drugn drugn
+docker run -p ${PORT}:3000 -d -v /etc/sysctl.conf:/etc/sysctl.conf -v "$(pwd)/external/expo-camera/build:/app/node_modules/expo-camera/build" --name drugn drugn
 docker rmi $(docker images -f dangling=true -q)
--- pro.Dockerfile
+++ pro.Dockerfile
@@ -1,3 +1,10 @@
+FROM node:16-alpine3.17 as drugn-build
+LABEL app="drugn"
+MAINTAINER tonmoy
+WORKDIR /app
+COPY . /app
+RUN yarn install
+RUN npx expo export:web
 FROM node:16-alpine3.17 as main
 WORKDIR /web-build
 COPY --from=drugn-build /app/web-build /web-build
